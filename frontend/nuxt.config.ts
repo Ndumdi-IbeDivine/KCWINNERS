fbc9dfcff5315bb1061a96f92070a992823b5718
Joshua-Nweze <joshuanweze270@gmail.com>
2025-08-29 13:22:40 +0100

    set ssr to false

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
     compatibilityDate: "2025-05-15",
     css: ["~/assets/css/main.css"],
+    ssr: false,
 
     plugins: ["~/plugins/aos.client.ts"],
 
